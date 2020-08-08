24caff5ccdd9d8bf8b20ad1025dd82a1ee9832a7
xiaochong2021 <a811159720@qq.com>
2020-08-08 19:38:05 +0800

    登录页面静态制作、input的数据校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,18 +1,212 @@
 <template>
   <div id="login">
-    这里是登录页面
-    <el-button>默认按钮</el-button>
-    <el-button type="primary">主要按钮</el-button>
-    <el-button type="success">成功按钮</el-button>
-    <el-button type="info">信息按钮</el-button>
-    <el-button type="warning">警告按钮</el-button>
-    <el-button type="danger">危险按钮</el-button>
+    <div class="login-wrap">
+      <ul class="menu-tap">
+        <li
+          :class="{current:item.current}"
+          @click="toggleMenu(item)"
+          v-for="(item,index) in menuTab"
+          :key="index"
+        >{{item.text}}</li>
+      </ul>
+      <!--表单-->
+      <el-form
+        :model="ruleForm"
+        status-icon
+        :rules="rules"
+        ref="ruleForm"
+        class="login-form"
+        size="mini"
+      >
+        <el-form-item prop="username" class="item-form">
+          <label>邮箱</label>
+          <el-input type="text" v-model="ruleForm.username" autocomplete="off"></el-input>
+        </el-form-item>
+
+        <el-form-item prop="password" class="item-form">
+          <label>密码</label>
+          <el-input
+            type="password"
+            v-model="ruleForm.password"
+            autocomplete="off"
+            maxlength="20"
+            minlength="6"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item prop="passwords" class="item-form" v-show="model === 'register'">
+          <label>重复密码</label>
+          <el-input
+            type="password"
+            v-model="ruleForm.passwords"
+            autocomplete="off"
+            maxlength="20"
+            minlength="6"
+          ></el-input>
+        </el-form-item>
+
+        <el-form-item prop="code" class="item-form">
+          <label>验证码</label>
+          <el-row :gutter="10">
+            <el-col :span="15">
+              <el-input v-model="ruleForm.code" maxlength="6" minlength="6"></el-input>
+            </el-col>
+            <el-col :span="9">
+              <el-button type="success" class="block">获取验证码</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="danger" @click="submitForm('ruleForm')" class="block login-btn">提交</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
-<style scoped></style>
+<script>
+import {
+  stripscript,
+  validateEmail,
+  validatePwd,
+  validateC
+} from "@/utils/validate.js";
+export default {
+  name: "login",
+  data() {
+    //验证用户名为邮箱
+    var validateUsername = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入用户名"));
+      } else if (!validateEmail(value)) {
+        callback(new Error("用户名格式有误"));
+      } else {
+        callback();
+      }
+    };
+    //验证密码
+    var validatePassword = (rule, value, callback) => {
+      this.ruleForm.password = stripscript(value);
+      value = this.ruleForm.password;
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else if (validatePwd(value)) {
+        callback(new Error("密码为6至20位的数字加字母"));
+      } else {
+        callback();
+      }
+    };
+
+    //验证重复密码
+    var validatePasswords = (rule, value, callback) => {
+        if(this.model === "login") { callback();}
+      this.ruleForm.passwords = stripscript(value);
+      value = this.ruleForm.passwords;
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value != this.ruleForm.password) {
+        callback(new Error("重复密码不正确"));
+      } else {
+        callback();
+      }
+    };
+    //验证验证码
+    var checkAge = (rule, value, callback) => {
+      this.ruleForm.code = stripscript(value);
+      value = this.ruleForm.code;
+
+      if (value === "") {
+        return callback(new Error("请输入验证码"));
+      } else if (validateC(value)) {
+        return callback(new Error("验证码格式有误"));
+      } else {
+        callback();
+      }
+    };
+
+    return {
+      menuTab: [{ text: "登录", current:true,type:'login' }, { text: "注册" , current:false,type:'register'}],
+      isActive: 0,
+      //模块的值
+      model: "login",
+      ruleForm: {
+        username: "",
+        password: "",
+        passwords: "",
+        code: ""
+      },
+      rules: {
+        username: [{ validator: validateUsername, trigger: "blur" }],
+        password: [{ validator: validatePassword, trigger: "blur" }],
+        passwords: [{ validator: validatePasswords, trigger: "blur" }],
+        code: [{ validator: checkAge, trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    toggleMenu(data) {
+      this.menuTab.forEach((elem,index) => {
+          elem.current = false;
+      })
+      //高光
+      data.current = true;
+      //修改模块值
+      this.model = data.type;
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    }
+  }
+};
+</script>
 <style lang="scss" scoped>
 #login {
   height: 100vh;
   background-color: #344a5f;
+}
+.login-wrap {
+  width: 300px;
+  margin: auto;
+}
+.menu-tap {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 88px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #fff;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+}
+.current {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+.login-form {
+  margin-top: 29px;
+  label {
+    display: block;
+    margin-bottom: 3px;
+    font-size: 14px;
+    color: #fff;
+  }
+}
+.item-form {
+  margin-bottom: 13px;
+}
+.block {
+  width: 100%;
+  display: block;
+}
+.login-btn {
+  margin-top: 19px;
 }
 </style>
